e99a8791ff7b1ba8149b38b417f89fabf9279b49
khomin <khomin@khomin-VirtualBox>
2018-06-03 17:41:17 +0300

    Main file and logic thread has been edited

--- src/I2cpoll.cpp
+++ src/I2cpoll.cpp
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-I2c_poll::I2c_poll() {    
+I2cPoll::I2cPoll() {
     db = new DbConnection();
     if(db->isConnecting()) {
         fprintf(stdout, "is connected\r\n");
@@ -20,53 +20,18 @@ I2c_poll::I2c_poll() {
     this->currentDeviceType = startDeviceType;
 }
 
-bool I2c_poll::readWord(char *cmd_command, uint16_t *p_word) {
-    int res = false;
-    int word = 0;
-    FILE *stream;
-    // отправляем в консоль
-    stream = popen(cmd_command, "r");
-    fprintf(stdout, "cmd_command - %s\r\n", cmd_command);
-    
-    std::string data;
-    if (stream) {
-        char reply_buff[128] = {0};
-        while (!feof(stream))
-        if(fgets(reply_buff, sizeof(reply_buff), stream) != NULL) {
-            data.append(reply_buff);
-        }
-        pclose(stream);
-    }
-    
-    // проверяем ответ
-//    data.insert(0, "I will read from device file /dev/i2c-0,\r\n0x401d\r\n");
-    fprintf(stdout, "shell result :\r\n%s\r\n", data.c_str());
-    if(data.length() != 0) {
-        char *p_start = strstr((char*)data.c_str(), "0x");
-        if(p_start != NULL) {
-            res = sscanf(p_start, "%x", &word);
-            if(res) {
-                *p_word = (uint16_t)word;
-            }
-            fprintf(stdout, "getWord %x\r\n", *p_word);
-        }
-    }
-    return res;
-}
-
-void* I2c_poll::pollExect() {
-//    FILE *stream;
+void I2cPoll::pollExect() {
     uint8_t *p_data = NULL;
     uint16_t word = 0;
     char cmd_command[128] = {0};
-    S_insertData device_data;    
+    S_insertData device_data;
     int device_addr = 0;
     // формируем запрос
     device_data.device_type = (E_I2c_device)currentDeviceType;
     switch(currentDeviceType) {
-        
+
         case dev_i2c_tmp112:
-            
+
             device_addr = db->getDeviceAddrFromName(i2c_Dev_typeName.tmp112.name_text.c_str());
             if(device_addr != 0) {
                 sprintf(cmd_command, "i2cget -y 0 0x%X 0x00 w\r\n", device_addr); //i2cget 0 0x48 0x00 w
@@ -82,24 +47,24 @@ void* I2c_poll::pollExect() {
                 }
             }
             break;
-            
+
         case dev_i2c_ina260:
             device_addr = db->getDeviceAddrFromName(i2c_Dev_typeName.ina260.name_text.c_str());
-            if(device_addr != 0) {                
-                for(int i=0; i<3; i++) {                    
+            if(device_addr != 0) {
+                for(int i=0; i<3; i++) {
                         switch(i) { // cur
                         case 0: //{i2cget 0 0x40 0x01 w}
                         sprintf(cmd_command, "i2cget -y 0 0x%X 0x01 w\r\n", device_addr);
                         // отправляем в консоль
                         if(readWord(cmd_command, &word)) {  // проверяем ответ
                             p_data = (uint8_t*)&word;
-                            float raw_data; 
+                            float raw_data;
                             fprintf(stdout, "Raw u16 %x\r\n", word);
                             raw_data = (0xFF & p_data[1]) | ((0xFF & (p_data[0])) << 8);
                             device_data.parameter.power_monitor.currnetFlowing = raw_data * 1.25 / 1000;
                             fprintf(stdout, "Current %4.2f\r\n", device_data.parameter.power_monitor.currnetFlowing);
                         }
-                        break;    
+                        break;
                         // voltage
                         case 1: //{i2cget 0 0x40 0x02 w}
                         sprintf(cmd_command, "i2cget -y 0 0x%X 0x02 w", device_addr);
@@ -113,7 +78,7 @@ void* I2c_poll::pollExect() {
                             fprintf(stdout, "Volage %4.2f\r\n", device_data.parameter.power_monitor.voltage);
                         }
                         break;
-                                                    
+
                         case 2: //{i2cget 0 0x40 0x03 w}
                         //  power
                         sprintf(cmd_command, "i2cget -y 0 0x%X 0x03 w\r\n", device_addr);
@@ -127,12 +92,12 @@ void* I2c_poll::pollExect() {
                             fprintf(stdout, "Power %4.2f\r\n", device_data.parameter.power_monitor.currentPower);
                         }
                         break;
-                    }   
+                    }
                 }
                 db->insertData(device_data);
             }
             break;
-            
+
             case dev_i2c_lis3dh:
 //                int value;
 //                uint8_t *valueL = (uint8_t*)(&value);
@@ -162,14 +127,14 @@ void* I2c_poll::pollExect() {
 //
 //                buff->AXIS_Z = value;
 //
-//                return MEMS_SUCCESS; 
-//                
-//                if(response != MEMS_ERROR) {
-//                    //DBGLog("Accel: X=%6d Y=%6d Z=%6d", data.AXIS_X, data.AXIS_Y, data.AXIS_Z);				
+//                return MEMS_SUCCESS;
 //
-//                    accX = ((float)data.AXIS_X)*2*9.81/32768; 
+//                if(response != MEMS_ERROR) {
+//                    //DBGLog("Accel: X=%6d Y=%6d Z=%6d", data.AXIS_X, data.AXIS_Y, data.AXIS_Z);
+//
+//                    accX = ((float)data.AXIS_X)*2*9.81/32768;
 //                    accY = ((float)data.AXIS_Y)*2*9.81/32768;
-//                    accZ = ((float)data.AXIS_Z)*2*9.81/32768; 				
+//                    accZ = ((float)data.AXIS_Z)*2*9.81/32768;
 //
 //                    accelTestStruct.data.AXIS_X = data.AXIS_X;
 //                    accelTestStruct.data.AXIS_Y = data.AXIS_Y;
@@ -182,17 +147,17 @@ void* I2c_poll::pollExect() {
 //                    }else{
 //                            accelTestStruct.enabled = true;
 //                    }
-//                }            
+//                }
             break;
-            
+
         case dev_i2c_txs02324:
             break;
-            
-        default: 
+
+        default:
             currentDeviceType = startDeviceType;
             break;
     }
-       
+
     if(currentDeviceType >= (E_I2c_device)endTypeDeviceType) {
         currentDeviceType = startDeviceType;
         fprintf(stdout, "I2c parce -endDev\r\n");
@@ -200,10 +165,42 @@ void* I2c_poll::pollExect() {
         currentDeviceType++;
         fprintf(stdout, "I2c parce -nextDev\r\n");
     }
-    return 0;
 }
 
-I2c_poll::~I2c_poll() {
+bool I2cPoll::readWord(char *cmd_command, uint16_t *p_word) {
+    int res = false;
+    int word = 0;
+    FILE *stream;
+    // отправляем в консоль
+    stream = popen(cmd_command, "r");
+    fprintf(stdout, "cmd_command - %s\r\n", cmd_command);
+
+    std::string data;
+    if (stream) {
+        char reply_buff[128] = {0};
+        while (!feof(stream))
+        if(fgets(reply_buff, sizeof(reply_buff), stream) != NULL) {
+            data.append(reply_buff);
+        }
+        pclose(stream);
+    }
+
+    // проверяем ответ
+    fprintf(stdout, "shell result :\r\n%s\r\n", data.c_str());
+    if(data.length() != 0) {
+        char *p_start = strstr((char*)data.c_str(), "0x");
+        if(p_start != NULL) {
+            res = sscanf(p_start, "%x", &word);
+            if(res) {
+                *p_word = (uint16_t)word;
+            }
+            fprintf(stdout, "getWord %x\r\n", *p_word);
+        }
+    }
+    return res;
+}
+
+I2cPoll::~I2cPoll() {
 	// TODO Auto-generated destructor stub
 }
 
